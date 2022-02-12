56681ff46e19f6f9a12e9f63cb1a848c815b7dba
Jkutkut <jorgeregonzalez@gmail.com>
2022-02-12 19:50:17 +0100

    fixing some bugs

--- commitMaker.sh
+++ commitMaker.sh
@@ -7,12 +7,12 @@ README_NAME="README.md"
 README="res/$README_NAME"
 COMMIT_MESSAGE="this commit is made by commitMaker.sh. Find out more at https://github.com/Jkutkut/GitHub-Contribution_Message"
 
+cp $README $REPO$README_NAME
 
 cd $REPO
 
-cp $README $REPO$README_NAME
-
-awk '{print $0}' testing/commits | while read c; do
+awk '{print $0}' $COMMIT_DATE_FILE | while read c; do
+	echo >> $README_NAME
 	git add $README_NAME > /dev/null
 	git commit -m "$COMMIT_MESSAGE" --date="$c" > /dev/null
 done
