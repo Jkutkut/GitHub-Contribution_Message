3a871773a05414f0a758cd874df8e1319107aa3a
Jkutkut <jorgeregonzalez@gmail.com>
2022-02-12 20:37:15 +0100

    command added to change the user and mail

--- scripts/tests/changeAllDates.sh
+++ scripts/tests/changeAllDates.sh
@@ -5,3 +5,4 @@ echo "Changing dates of all commits in the current branch"
 newDate="Sat Feb 12 2022 22:42:42 +0100"
 
 git rebase -i --exec "git commit --amend --date=\"$newDate\" --no-edit" --root
+# git rebase -i --exec "git commit --amend --author=\"jkutkutTesting <jorgeregonzalezetsit@gmail.com>\" --no-edit" --root
