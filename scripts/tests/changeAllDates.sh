1d9d8a0fd7364bb9f9a7b1363e8d8494cfdf8690
Jkutkut <jorgeregonzalez@gmail.com>
2022-02-12 19:26:54 +0100

    date now correct format

--- scripts/tests/changeAllDates.sh
+++ scripts/tests/changeAllDates.sh
@@ -2,6 +2,6 @@
 
 echo "Changing dates of all commits in the current branch"
 
-newDate="Sat 2022 22:42:42 +0100"
+newDate="Sat Feb 12 2022 22:42:42 +0100"
 
 git rebase -i --exec "git commit --amend --date=\"$newDate\" --no-edit" --root
