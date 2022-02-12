09fe5d33929e8a65be3f1598a424ef7ae96b5304
Jkutkut <jorgeregonzalez@gmail.com>
2022-02-12 16:02:38 +0100

    updated to work with arguments

--- contributionMessage.py
+++ contributionMessage.py
@@ -6,16 +6,26 @@ from scripts.text2font import *
 
 text: str = ""
 font: str = "./fonts/py_basic_font.json"
+format: bool = True
 
 i: int = 1
 while i < len(sys.argv):
 	if sys.argv[i] == "--font":
 		font = sys.argv[i+1]
 		i += 1
+	elif sys.argv[i] == "--no-format":
+		format = False
+	elif sys.argv[i] == "--format":
+		format = True
 	else:
 		text += sys.argv[i]
 	i += 1
 
-print("\n*****************************")
 print(f"Information:\n\t - Font: {font}\n\t - Text: \"{text}\"")
-print("\n*****************************")
+
+t = Text2font.text2font(text, font)
+if not t is None:
+	if format:
+		print (t.replace("0", " "))
+	else:
+		print(t)
