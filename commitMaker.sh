e77b898bdfc1a7d4ae2e4afe9b8d0d3232f6efea
Jkutkut <jorgeregonzalez@gmail.com>
2022-02-13 11:49:32 +0100

    works nice now

--- commitMaker.sh
+++ commitMaker.sh
@@ -6,15 +6,27 @@ REPO="testing/repo/"
 README_NAME="README.md"
 README="res/$README_NAME"
 COMMIT_MESSAGE="this commit is made by commitMaker.sh. Find out more at https://github.com/Jkutkut/GitHub-Contribution_Message"
+USER_MAIL="jkutkutTesting <jorgeregonzalezetsit@gmail.com>"
+REMOTE_URL="https://github.com/JkutkutTesting/repo.git"
+
+
+rm -rf $REPO # Remove previous repo
+
+mkdir $REPO
 
 cp $README $REPO$README_NAME
 
 cd $REPO
 
+git init
+git remote add origin $REMOTE_URL
+
 awk '{print $0}' $COMMIT_DATE_FILE | while read c; do
-	echo >> $README_NAME
+	echo -n " " >> $README_NAME
 	git add $README_NAME > /dev/null
-	git commit -m "$COMMIT_MESSAGE" --date="$c" > /dev/null
+	git commit -m "$COMMIT_MESSAGE" --date="$c" --author="$USER_MAIL" > /dev/null
 done
+
+git push --force --set-upstream origin main
 
 cd - > /dev/null
