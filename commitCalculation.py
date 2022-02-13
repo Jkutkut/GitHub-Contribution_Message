88a840dd517dc537e410fad68f4a7b9e11630eaf
Jkutkut <jorgeregonzalez@gmail.com>
2022-02-13 11:28:37 +0100

    dates should be fixed now

--- commitCalculation.py
+++ commitCalculation.py
@@ -21,19 +21,25 @@ endDate = endDate.replace(hour=0, minute=42, second=42)
 
 # Analyse the data
 endDateWD = endDate.weekday()
-firstMonday = endDate - timedelta(days=endDateWD + 7) - timedelta(days=7 * len(data[0]))
+firstSunday = endDate - timedelta(days=(endDateWD + 1) % 7) # Current week's Sunday
+firstSunday = firstSunday - timedelta(days=7) # Previous week's Sunday
+firstSunday = firstSunday - timedelta(days=7 * (len(data[0]) - 1)) # First Sunday
 
 if debug:
 	print(f"End date: {endDate.strftime('%d/%m/%y')}")
 	dataStr = "\n".join(data)
 	print(f"Data:\n{dataStr}")
 	print(f"End date weekday: {endDateWD}")
-	print(f"First Monday: {firstMonday.strftime('%d/%m/%y')}")
+	print(f"First Sunday: {firstSunday.strftime('%d/%m/%y')}")
+	print(f"Expected first Sunday: 0/0/2021")
 
-for r in range(len(data)):
-	for c in range(len(data[0])):
+for c in range(len(data[0])):
+	for r in range(len(data)):
 		if not data[r][c] == " ":
 			for commit in range(int(data[r][c])):
 				# Sat Feb 12 18:18:41 2022 +0100
-				d = firstMonday - timedelta(days=7*r + c)
+				d = firstSunday + timedelta(days=((r + 1) % 7) + c * 7)
 				print(f"{d.strftime('%a %b %d %H:%M:%S %Y +0100')}")
+	# 	print(data[r][c], end="")
+	# print()
+	
