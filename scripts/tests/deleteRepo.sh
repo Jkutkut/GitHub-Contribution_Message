b14be37c5fe874de8c88ac09242da3e467ddbb2f
Jkutkut <jorgeregonzalez@gmail.com>
2022-04-18 09:52:34 +0200

    deleteRepo refactored

--- scripts/tests/deleteRepo.sh
+++ scripts/tests/deleteRepo.sh
@@ -1,4 +1,12 @@
-repo="jkutkutTesting-repo"
-usr="jkutkutTesting"
-TOKEN=""
-curl -X DELETE -H "Authorization: token $TOKEN" "https://api.github.com/repos/$usr/$repo";
+repo="repo"
+. $PWD/.configuration
+
+echo "Deleting repo \"$repo\""
+curl -X DELETE -H "Authorization: token $REPO_TOKEN" "https://api.github.com/repos/$REPO_USR/$repo" > result.tmp
+
+if [ ! -s result.tmp ]; then
+	echo "Repo \"$repo\" deleted"
+else
+	echo "Repo \"$repo\" not deleted"
+fi
+rm -f result.tmp
