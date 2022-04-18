6aa5e65ee8ae968a586f8ea9b22e00643a7aa579
Jkutkut <jorgeregonzalez@gmail.com>
2022-04-18 10:11:14 +0200

    refactored

--- scripts/tests/createRepo.sh
+++ scripts/tests/createRepo.sh
@@ -1,7 +1,6 @@
 repo="repo"
-usr="jkutkutTesting"
-TOKEN=""
+. $PWD/.configuration
 
-curl -X POST -H "Authorization: token $TOKEN" https://api.github.com/user/repos --data "{\"name\":\"$repo\"}"
+curl -X POST -H "Authorization: token $REPO_TOKEN" https://api.github.com/user/repos --data "{\"name\":\"$repo\"}"
 
 # curl -H "Authorization: token ACCESS_TOKEN" --data '{"name":"NEW_REPO_NAME"}' https://api.github.com/orgs/ORGANIZATION_NAME/repos
